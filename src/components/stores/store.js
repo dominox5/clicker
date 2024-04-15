0526668ad38dca6348b7d088e999c91dfbf5aaed
Dominik Vokůrka <dominox@post.cz>
2024-04-15 22:12:00 +0200

    remove comments

--- src/components/stores/store.js
+++ src/components/stores/store.js
@@ -1,17 +1,7 @@
-/*import { defineStore } from 'pinia'
 
-export const useClicker = defineStore('clicker', {
-  // Default state / config
-  state: () => {
-    return {
-      balance: 0
-    }
-  }
-})*/
 import { defineStore } from 'pinia'
 
 export const useClicker = defineStore('clicker', {
-  // Default state / config
   state: () => {
     return {
       balance: parseInt(localStorage.getItem('clicker_balance')) || 0
